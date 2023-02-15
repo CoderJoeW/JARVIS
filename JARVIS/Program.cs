f9007d8c6cd10ecc432299e181a7bcb3009e6836
jwilliamson <j.williamson@gambler-labs.com>
2023-02-15 16:25:36 -0500

    seperate out the text to speech logic

--- JARVIS/Program.cs
+++ JARVIS/Program.cs
@@ -10,20 +10,23 @@ using Grpc.Net.Client;
 using JARVIS;
 using NAudio.Wave;
 
-await BeginBot();
+var config = Config.Load("config.json");
 
-static async Task BeginBot()
+var clientBuilder = new TextToSpeechClientBuilder
 {
+    CredentialsPath = config.GoogleCloudTextToSpeechKey
+};
+TextToSpeechClient client = await clientBuilder.BuildAsync();
+
+await BeginBot(client);
+
+static async Task BeginBot(TextToSpeechClient client)
+{
+    string opening = "Hello, My name is JARVIS. How can I help you today?";
+    Console.WriteLine(opening);
+    await SpeakText(client, opening);
+
     OpenAI openAI = new OpenAI(2000);
-
-    var config = Config.Load("config.json");
-
-    // Authenticate with Google Cloud using a service account
-    var clientBuilder = new TextToSpeechClientBuilder
-    {
-        CredentialsPath = config.GoogleCloudTextToSpeechKey
-    };
-    var client = await clientBuilder.BuildAsync();
 
     while (true)
     {
@@ -36,34 +39,7 @@ static async Task BeginBot()
         var output = await GenerateOutput(openAI, input);
         Console.WriteLine($"JARVIS: {output}");
 
-        var response = await client.SynthesizeSpeechAsync(new SynthesizeSpeechRequest
-        {
-            Input = new SynthesisInput
-            {
-                Text = output
-            },
-            Voice = new VoiceSelectionParams
-            {
-                LanguageCode = "en-US",
-                SsmlGender = SsmlVoiceGender.Male
-            },
-            AudioConfig = new AudioConfig
-            {
-                AudioEncoding = AudioEncoding.Linear16
-            }
-        });
-
-        using var audioStream = new MemoryStream(response.AudioContent.ToByteArray());
-        using var audioFile = new WaveFileReader(audioStream);
-        using var outputDevice = new WaveOutEvent();
-
-        outputDevice.Init(audioFile);
-        outputDevice.Play();
-
-        while (outputDevice.PlaybackState == PlaybackState.Playing)
-        {
-            await Task.Delay(100);
-        }
+        await SpeakText(client, output);
     }
 }
 
@@ -78,4 +54,36 @@ static async Task<string> GenerateOutput(OpenAI openAI, string prompt)
     var presencePenalty = 0.6;
 
     return await openAI.CreateCompletion(prompt, engine, maxTokens, temperature, topP, frequencyPenalty, presencePenalty, stop);
+}
+
+static async Task SpeakText(TextToSpeechClient client, string text)
+{
+    var response = await client.SynthesizeSpeechAsync(new SynthesizeSpeechRequest
+    {
+        Input = new SynthesisInput
+        {
+            Text = text
+        },
+        Voice = new VoiceSelectionParams
+        {
+            LanguageCode = "en-US",
+            SsmlGender = SsmlVoiceGender.Male
+        },
+        AudioConfig = new AudioConfig
+        {
+            AudioEncoding = AudioEncoding.Linear16
+        }
+    });
+
+    using var audioStream = new MemoryStream(response.AudioContent.ToByteArray());
+    using var audioFile = new WaveFileReader(audioStream);
+    using var outputDevice = new WaveOutEvent();
+
+    outputDevice.Init(audioFile);
+    outputDevice.Play();
+
+    while (outputDevice.PlaybackState == PlaybackState.Playing)
+    {
+        await Task.Delay(100);
+    }
 }
