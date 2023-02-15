22abf644c0d3b1e5cad98ee288346ecafe6523ff
jwilliamson <j.williamson@gambler-labs.com>
2023-02-15 17:50:15 -0500

    better keyword recognition

--- JARVIS/Program.cs
+++ JARVIS/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Speech.Recognition;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
 using Google.Cloud.TextToSpeech.V1;
 using Grpc.Auth;
 using Grpc.Core;
@@ -17,9 +19,9 @@ SpeechToText speechToText = new SpeechToText(config.GoogleCloudTextToSpeechKey);
 
 OpenAI openAI = new OpenAI(2000);
 
-await BeginBot(textToSpeech, speechToText, openAI);
+await BeginBot(textToSpeech, speechToText, openAI, config);
 
-static async Task BeginBot(JARVIS.TextToSpeech textToSpeech, SpeechToText speechToText, OpenAI openAI)
+static async Task BeginBot(JARVIS.TextToSpeech textToSpeech, SpeechToText speechToText, OpenAI openAI, Config config)
 {
     string opening = "Hello, My name is JARVIS. How can I help you today?";
     Console.WriteLine(opening);
@@ -35,7 +37,26 @@ static async Task BeginBot(JARVIS.TextToSpeech textToSpeech, SpeechToText speech
             continue;
         }
 
-        string input = await speechToText.RecognizeSpeechAsync(audioData);
+        Console.WriteLine("Checking for trigger word...");
+        var triggerWord = await RecognizeTriggerWord(audioData, config.GoogleCloudTextToSpeechKey);
+
+        if (triggerWord == null)
+        {
+            Console.WriteLine("Failed to recognize trigger word.");
+            continue;
+        }
+
+        Console.WriteLine($"Trigger word recognized: {triggerWord}");
+
+        byte[] remainingAudioData = await RecordAudioAsync();
+
+        if (remainingAudioData == null)
+        {
+            Console.WriteLine("Failed to recognize speech input.");
+            continue;
+        }
+
+        string input = await speechToText.RecognizeSpeechAsync(remainingAudioData);
 
         if (string.IsNullOrWhiteSpace(input))
         {
@@ -55,22 +76,64 @@ static async Task BeginBot(JARVIS.TextToSpeech textToSpeech, SpeechToText speech
     }
 }
 
+static async Task<string> RecognizeTriggerWord(byte[] audioData,string credentials)
+{
+    var client = new SpeechClientBuilder
+    {
+        CredentialsPath = credentials
+    }.Build();
+
+    var config = new RecognitionConfig
+    {
+        Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+        SampleRateHertz = 16000,
+        LanguageCode = "en-US",
+        SpeechContexts = {
+                    new SpeechContext
+                    {
+                        Phrases = { "jarvis" },
+                    },
+                   },
+    };
+    var audio = RecognitionAudio.FromBytes(audioData);
+    var response = await client.RecognizeAsync(config, audio);
+
+    if (response.Results.Count == 0)
+    {
+        return null;
+    }
+
+    return response.Results[0].Alternatives[0].Transcript.ToLower();
+}
+
 static async Task<byte[]> RecordAudioAsync()
 {
     using (var waveIn = new WaveInEvent())
     using (var memoryStream = new MemoryStream())
     {
+        bool isRecording = false;
+
+        // Start recording audio
         waveIn.WaveFormat = new WaveFormat(16000, 1);
 
         waveIn.DataAvailable += (sender, e) =>
         {
-            memoryStream.Write(e.Buffer, 0, e.BytesRecorded);
+            if (isRecording)
+            {
+                memoryStream.Write(e.Buffer, 0, e.BytesRecorded);
+            }
+        };
+
+        waveIn.RecordingStopped += (sender, e) =>
+        {
+            isRecording = false;
         };
 
         waveIn.StartRecording();
+        isRecording = true;
 
-        Console.WriteLine("Listening for input...");
-        await Task.Delay(TimeSpan.FromSeconds(5)); // Wait for 5 seconds of audio to be captured
+        // Wait for 5 seconds of audio to be captured
+        await Task.Delay(TimeSpan.FromSeconds(5));
 
         waveIn.StopRecording();
 
